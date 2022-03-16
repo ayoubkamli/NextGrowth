3569ff9058d0f62fa1e54c076b884d4d7d12329c
Ayyoub Kamli <akamli@e2r7p10.1337.ma>
2022-03-16 19:48:15 +0100

    Add table body from array

--- app.js
+++ app.js
@@ -1,12 +1,88 @@
 let value = false;
-const showModal = () => {
-    if (value){
-            document.getElementById("modal").classList.add("modal")
-            document.getElementById("backdrop").classList.add("modal")
-    } else {
-        document.getElementById("modal").classList.remove("modal")
-        document.getElementById("backdrop").classList.remove("modal")
 
-    }
-    value = !value;
+let users = [
+  {
+    id: "123456789",
+    createdDate: "2021-01-06T00:00:00.000Z",
+    status: "En validation",
+    firstName: "Mohamed",
+    lastName: "Taha",
+    userName: "mtaha",
+    registrationNumber: "2584",
+  },
+  {
+    id: "987654321",
+    createdDate: "2021-07-25T00:00:00.000Z",
+    status: "Validé",
+    firstName: "Hamid",
+    lastName: "Orrich",
+    userName: "horrich",
+    registrationNumber: "1594",
+  },
+  {
+    id: "852963741",
+    createdDate: "2021-09-15T00:00:00.000Z",
+    status: "Rejeté",
+    firstName: "Rachid",
+    lastName: "Mahidi",
+    userName: "rmahidi",
+    registrationNumber: "3576",
+  },
+];
+
+const addTable = () => {
+  var table = document.getElementById('users').getElementsByTagName('tbody')[0];
+  let validationClass;
+  for (let index = 0; index < users.length; index++){
+      if (users[index].status === 'Validé') {
+        validationClass = "valide"
+      } else if (users[index].status === 'Rejeté'){
+        validationClass = "rejete"
+      } else {
+        validationClass = "en-validation"
+      }
+      table.insertRow().innerHTML = "<td>" + users[index].id + "</td>" + 
+      "<td>" + users[index].createdDate.split('T')[0] + "</td>" + 
+      "<td><div class='state " + validationClass + "'>" + users[index].status + "</div></td>" +
+      "<td>" + users[index].lastName + "</td>" + 
+      "<td>" + users[index].firstName + "</td>" + 
+      "<td>" + users[index].userName + "</td>" + 
+      "<td>" + users[index].registrationNumber + "</td>" + 
+      "<td><i style='font-size: 24px' class='fa'>&#xf014;</i></td>";
+  }
+  console.log(table);
+};
+addTable();
+
+const addRow = (e) => {
+    var table = document.getElementById('users').getElementsByTagName('tbody')[0];
+  let validationClass;
+  if (users[1].status === 'Validé') {
+    validationClass = "valide"
+  } else if (users[1].status === 'Rejeté'){
+    validationClass = "rejete"
+  } else {
+    validationClass = "en-validation"
+  }
+
+  table.insertRow().innerHTML = "<td>" + users[1].id + "</td>" + 
+      "<td>" + users[1].createdDate.split('T')[0] + "</td>" + 
+      "<td><div class='state " + validationClass + "'>" + users[1].status + "</div></td>" +
+      "<td>" + users[1].lastName + "</td>" + 
+      "<td>" + users[1].firstName + "</td>" + 
+      "<td>" + users[1].userName + "</td>" + 
+      "<td>" + users[1].registrationNumber + "</td>" + 
+      "<td><i style='font-size: 24px' class='fa'>&#xf014;</i></td>";
+
 }
+
+const showModal = () => {
+  if (value) {
+    document.getElementById("modal").classList.add("modal");
+    document.getElementById("backdrop").classList.add("modal");
+  } else {
+    document.getElementById("modal").classList.remove("modal");
+    document.getElementById("backdrop").classList.remove("modal");
+  }
+  value = !value;
+};
